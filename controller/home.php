d57201125e2c507086fd0958679d829120a66200
camille <138686617+camillelimade@users.noreply.github.com>
2025-11-12 11:17:12 -0300

    style: estilização da home

--- controller/home.php
+++ controller/home.php
@@ -11,11 +11,54 @@
 <style>
     body{
         text-align: center;
+        background-color: #1c1b1bff;
     }#corpo1{
-        height: 500px;width:500px;
+        height: 500px;
+        width:500px;
+        padding-top: 55px;
+    }
+    h1, p, hr{
+        color: whitesmoke;
     }
 </style>
 <body>
+    <nav class="navbar navbar-dark fixed-top" style="background-color: #181616ff;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="home.php">Painel de Cadastros</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation" style="background-color: #181616ff;">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel" >
+      <div class="offcanvas-header" style="background-color: #181616ff;">
+        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Lista de Ferramentas</h5>
+        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </a>
+            <ul class="dropdown-menu dropdown-menu-dark">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li>
+                <hr class="dropdown-divider">
+              </li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
     <div class="container" id="corpo1">
         <div class="row">
             <div class="col">
@@ -24,14 +67,13 @@
                     <p class="lead">Este é um sistema básico da realização de cadastros. Base de estudos para a criação de sistemas Web com PHP e MySQL.</p>
                     <hr class="my-4">
                     <p>Acesse nossas funcionalidades.</p>
-                    <a href="cad.php" class="btn btn-dark btn-lg" role="button">Cadastrar</a>
-                    <a href="pesquisa.php" class="btn btn-dark btn-lg" role="button">Pesquisar</a>
+                    <a href="cad.php" class="btn btn-outline-light btn-lg" role="button">Cadastrar</a>
+                    <a href="pesquisa.php" class="btn btn-outline-light btn-lg" role="button">Pesquisar</a>
                 </div>
             </div>
         </div>
 
     </div>
-    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="../js/bootstrap.bundle.min.js"></script>
 </body>
-
 </html>
